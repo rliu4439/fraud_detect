8dde412faa89272496e2e9d453d07dcaf3bd801f
Ryan <rliu4439@berkeley.edu>
2021-12-28 15:58:34 -0800

    Update flask api

--- app/app.py
+++ app/app.py
@@ -1,20 +1,26 @@
 from flask import Flask, request
 import tensorflow
 import numpy as np
+from pickle import load
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Python11!"
+    return "Hello!"
 
 @app.route('/score', methods=['POST'])
 def score():
     
     n_inputs = 30
     oversample_model = tensorflow.keras.models.load_model('saved_model/my_model')
+    rob_scaler = load(open('scaler.pkl', 'rb'))
 
+    # X is array of 28 elems, [V1..V28]
     features = request.json['X']
+    #amount_time is [amount,time]
+    amount_time=request.json['amount_time']
+    features=amount_time.extend(features)
     predictions=oversample_model.predict(features)
-    result = np.argmax(oversample_predictions,axis=1)
+    result = np.argmax(predictions,axis=1)
     return 'fraud' if result==0 else 'not fraud'
